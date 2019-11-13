865e3ad29d4494e0ebe7e673751fbf12cd8baf88
yuce <yuce@jd.com>
2019-11-13 17:48:29 +0800

    webpack config

---
+++ src/main.js
@@ -0,0 +1,18 @@
+require("expose-loader?$!jquery")
+import './styles/reset.scss'
+
+// 测试 ES6 语法是否通过 babel 转译
+const array = [1, 2, 3]
+const isES6 = () => console.log(...array)
+
+isES6()
+
+const arr = [new Promise(() => {}), new Promise(() => {})]
+
+arr.map(item => {
+  console.log(item)
+})
+
+$(function () {
+  console.log($('#app'))
+})
