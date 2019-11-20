be96544d0b9b0dca940c73e88934e4c39909ca72
yuce <yuce@jd.com>
2019-11-20 13:57:03 +0800

    deploy

--- src/templates/header/index.js
+++ src/templates/header/index.js
@@ -1,6 +1,7 @@
 let headerStr = `
-  <div class='saas-header'></div>
-  <a href='/index2.html'>index2.html</a>
+  <div class='saas-header'>
+    <div class="header-left">header</div>
+  </div>
 `
 
 document.getElementById('saas-container').innerHTML += headerStr
