5af3b61b3c6dd51391ef8b08d164b9513ba8404e
yuce <yuce@jd.com>
2019-11-27 14:59:48 +0800

    add menu.art

--- src/main.js
+++ src/main.js
@@ -1,18 +1,6 @@
 // require("expose-loader?$!jquery")
 import './styles/index.scss'
 
-// 测试 ES6 语法是否通过 babel 转译
-// const array = [1, 2, 3]
-// const isES6 = () => console.log(...array)
-//
-// isES6()
-//
-// const arr = [new Promise(() => {}), new Promise(() => {})]
-//
-// arr.map(item => {
-//   console.log(item)
-// })
-
 $(function () {
   const headerRender = require('./templates/header.art')
   const headerData = {
